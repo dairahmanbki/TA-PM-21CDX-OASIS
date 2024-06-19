89c7e84040c164c62a33c56e10906e6c29b5eff3
dairahmanbki <dairahmanbakti23@gmail.com>
2024-06-19 15:53:19 +0700

    master: Membuat atau Menambahkan onclik ke button login

--- Program/app/src/main/java/com/uti/oasis/MainActivity.kt
+++ Program/app/src/main/java/com/uti/oasis/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.uti.oasis
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+    fun onLoginButtonClick(view: View?) {
+        // Lakukan aksi saat tombol login diklik
+        Toast.makeText(this, "Login button clicked", Toast.LENGTH_SHORT).show()
+        // Anda bisa menambahkan kode lain di sini, seperti melakukan validasi atau berpindah ke Activity lain
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
